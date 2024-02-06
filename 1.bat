7444b5fb76d8334cccaf3db44d08ec981d3726a8
airjuice1 <airjuice1@gmail.com>
2024-02-06 04:26:52 +0300

    modify 1.bat add delete snippet files

--- 1.bat
+++ 1.bat
@@ -1,3 +1,5 @@
+del /S "%APPDATA%\\Sublime Text\\Packages\\User\\*.sublime-snippet"
+timeout /t 5 /nobreak
 xcopy .\*.* "%APPDATA%\Sublime Text\Packages\User" /R /I /S /Y /EXCLUDE:excludedfileslist.txt
 mkdir "%HOMEPATH%\\Documents\\webapps"
 mkdir "%HOMEPATH%\\Documents\\workspaces"
