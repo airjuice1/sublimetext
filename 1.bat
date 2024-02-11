91a0b27d149d6716691fa24859ca5892ec3b3cc7
airjuice1 <airjuice1@gmail.com>
2024-02-11 11:27:44 +0300

    some changes

--- 1.bat
+++ 1.bat
@@ -2,6 +2,6 @@ del /S "%APPDATA%\\Sublime Text\\Packages\\User\\*.sublime-snippet"
 timeout /t 1 /nobreak
 xcopy .\*.* "%APPDATA%\Sublime Text\Packages\User" /R /I /S /Y /EXCLUDE:excludedfileslist.txt
 mkdir "%HOMEPATH%\\Documents\\webapps"
-mkdir "%HOMEPATH%\\Documents\\workspaces"
+rem mkdir "%HOMEPATH%\\Documents\\workspaces"
 rem mkdir "%HOMEPATH%\\Documents\\webapps\\.sublime"
 PAUSE
