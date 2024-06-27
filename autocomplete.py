5707c8c17bac9d57f963e09868ed171ddd2affd2
airjuice1 <airjuice1@gmail.com>
2024-06-27 11:05:22 +0300

    simple autocomplete laravel v2

--- autocomplete.py
+++ autocomplete.py
@@ -28,6 +28,22 @@ def _extract_class(text):
 	if matches_namespace and matches_class:
 		return matches_namespace[0] + '\\' + matches_class[0]
 
+def _find_postion_in_text(text):
+
+	result = False
+
+	pattern = r';([^;]*?)\s*class'
+	matches = re.search(pattern, text)
+
+	if matches:
+		result = matches.start()
+
+	return result
+
+class InsertTextAtPositionCommand(sublime_plugin.TextCommand):
+	def run(self, edit, position, text):
+		self.view.insert(edit, position, text)
+
 class SimpleLaravelAutoCompleteCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		prj_path = sublime.active_window().folders()[0]
@@ -41,6 +57,14 @@ class SimpleLaravelAutoCompleteCommand(sublime_plugin.TextCommand):
 		if index == -1:
 			return
 
+		text = self.view.substr(sublime.Region(0, self.view.size()))
+
+		position = _find_postion_in_text(text)
 		selected_item = 'use ' + self.elements[index] + ';\n'
-		sublime.set_clipboard('use ' + selected_item + ';\n')
-		self.view.run_command("insert_snippet", {"contents": selected_item})
+		# sublime.set_clipboard('use ' + selected_item + ';\n')
+		# self.view.run_command("insert_snippet", {"contents": selected_item})
+
+		if position:
+			self.view.run_command("insert_text_at_position", {"position": position + 2, "text": selected_item})
+		else:
+			print('class not found')
