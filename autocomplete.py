1cda45f2dd39f6f0cd822ccb6044ead023c73096
airjuice1 <airjuice1@gmail.com>
2024-06-27 12:09:32 +0300

    simple autocomplete laravel v2.1

--- autocomplete.py
+++ autocomplete.py
@@ -5,7 +5,7 @@ import re
 
 def _read_files(pathes, project_path):
 	except_files = ['.gitignore', 'database.sqlite']
-	result = []
+	result = [[],[]]
 	for path in pathes:
 		for root, dirs, files in os.walk(path):
 			for file in files:
@@ -17,7 +17,8 @@ def _read_files(pathes, project_path):
 				with open(file_path, encoding='utf-8') as f:
 					code = _extract_class(f.read())
 					if code:
-						result.append(code)
+						result[0].append(code[0])
+						result[1].append(code[1])
 	return result
 
 def _extract_class(text):
@@ -26,13 +27,13 @@ def _extract_class(text):
 	matches_namespace = re.findall(patten_namespace, text)
 	matches_class = re.findall(patten_class, text)
 	if matches_namespace and matches_class:
-		return matches_namespace[0] + '\\' + matches_class[0]
+		return [matches_namespace[0] + '\\' + matches_class[0], matches_class[0]]
 
 def _find_postion_in_text(text):
 
 	result = False
 
-	pattern = r';([^;]*?)\s*class'
+	pattern = r';([^;]*?)\s*(?:class|Route)'
 	matches = re.search(pattern, text)
 
 	if matches:
@@ -51,7 +52,7 @@ class SimpleLaravelAutoCompleteCommand(sublime_plugin.TextCommand):
 		dba_path = os.path.join(prj_path, "database")
 
 		self.elements = _read_files([app_path, dba_path], prj_path)
-		self.view.window().show_quick_panel(self.elements, self.on_done)
+		self.view.window().show_quick_panel(self.elements[0], self.on_done)
 
 	def on_done(self, index):
 		if index == -1:
@@ -60,11 +61,12 @@ class SimpleLaravelAutoCompleteCommand(sublime_plugin.TextCommand):
 		text = self.view.substr(sublime.Region(0, self.view.size()))
 
 		position = _find_postion_in_text(text)
-		selected_item = 'use ' + self.elements[index] + ';\n'
+		selected_item = 'use ' + self.elements[0][index] + ';\n'
+		selected_class = self.elements[1][index]
 		# sublime.set_clipboard('use ' + selected_item + ';\n')
-		# self.view.run_command("insert_snippet", {"contents": selected_item})
 
 		if position:
+			self.view.run_command("insert_classname_at_position", {"text": selected_class})
 			self.view.run_command("insert_text_at_position", {"position": position + 2, "text": selected_item})
 		else:
 			print('class not found')
